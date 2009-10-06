7aa1c620d3cafe2d30feab14e44f43063cb5ea8f
Ilja Booij <ibooij@gmail.com>
2009-10-06 17:33:47 +0200

    Added test for TcxImporterImpl.

--- src/main/java/nl/iljabooij/garmintrainer/parser/digester/ParseException.java
+++ src/main/java/nl/iljabooij/garmintrainer/parser/digester/ParseException.java
@@ -21,6 +21,10 @@ package nl.iljabooij.garmintrainer.parser.digester;
 public class ParseException extends Exception {
 	private static final long serialVersionUID = 1L;
 
+	public ParseException(final String message) {
+		super(message);
+	}
+	
 	public ParseException(final String message, final Throwable throwable) {
 		super(message, throwable);
 	}
