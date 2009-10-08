121774f0542f6c7f7dbc884c564305a7dbc75b42
Ilja Booij <ibooij@gmail.com>
2009-10-08 16:00:19 +0200

    [Minor] used Comparator in TrackPoint instead of Ordering. ActivityImpl now uses Ordering.from() with the Comparators to get an ordering.

--- src/main/java/nl/iljabooij/garmintrainer/model/ActivityImpl.java
+++ src/main/java/nl/iljabooij/garmintrainer/model/ActivityImpl.java
@@ -34,25 +34,32 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Ordering;
 
 /**
- * Implementation of {@link Activity}. Note that instances of ActivityImpl
- * are immutable (hence the @Immutable annotation). 
+ * Implementation of {@link Activity}. Note that instances of ActivityImpl are
+ * immutable (hence the @Immutable annotation).
  * 
  * @author ilja
  */
 @Immutable
-public final class ActivityImpl implements Comparable<Activity>, Serializable, Activity {
-    private static final long serialVersionUID = 1L;
+public final class ActivityImpl implements Comparable<Activity>, Serializable,
+		Activity {
+	private static final long serialVersionUID = 1L;
 	private final DateTime startTime;
 	private final ImmutableList<Lap> laps;
-	
+
 	/**
-	 * Create a new {@link ActivityImpl}. 
-	 * @param startTime start time of the activity
-	 * @param laps laps in the activity, cannot be empty
-	 * @throws NullPointerException if any of the arguments are null
-	 * @throws IllegalArgumentException if laps is empty.
+	 * Create a new {@link ActivityImpl}.
+	 * 
+	 * @param startTime
+	 *            start time of the activity
+	 * @param laps
+	 *            laps in the activity, cannot be empty
+	 * @throws NullPointerException
+	 *             if any of the arguments are null
+	 * @throws IllegalArgumentException
+	 *             if laps is empty.
 	 */
 	public ActivityImpl(final DateTime startTime, final List<Lap> laps) {
 		this.startTime = checkNotNull(startTime);
@@ -67,47 +74,51 @@ public final class ActivityImpl implements Comparable<Activity>, Serializable, A
 	public DateTime getStartTime() {
 		return startTime;
 	}
-	
+
 	/** {@inheritDoc} */
 	public DateTime getEndTime() {
-		return getLastTrackPoint().getTime();	
+		return getLastTrackPoint().getTime();
 	}
-	
-	
+
 	/** {@inheritDoc} */
 	public Length getMaximumAltitude() {
-		return new TrackPoint.AltitudeOrdering().max(getTrackPoints()).getAltitude();
+		return Ordering.from(new TrackPoint.AltitudeComparator()).max(
+				getTrackPoints()).getAltitude();
 	}
-	
+
 	/** {@inheritDoc} */
 	public Length getMinimumAltitude() {
-		return new TrackPoint.AltitudeOrdering().min(getTrackPoints()).getAltitude();
+		return Ordering.from(new TrackPoint.AltitudeComparator()).min(
+				getTrackPoints()).getAltitude();
 	}
-	
+
 	/** {@inheritDoc} */
 	public Speed getMaximumSpeed() {
-		return new TrackPoint.SpeedOrdering().max(getTrackPoints()).getSpeed();
+		return Ordering.from(new TrackPoint.SpeedComparator()).max(
+				getTrackPoints()).getSpeed();
 	}
-	
+
 	/** {@inheritDoc} */
 	public Duration getGrossDuration() {
 		return new Duration(startTime, getLastTrackPoint().getTime());
 	}
-	
+
 	/** {@inheritDoc} */
 	public Duration getNetDuration() {
-		// start with the difference between start time and first lap start time.
-		Duration netDuration = new Duration(startTime, laps.get(0).getStartTime());
-		for (Lap lap: laps) {
+		// start with the difference between start time and first lap start
+		// time.
+		Duration netDuration = new Duration(startTime, laps.get(0)
+				.getStartTime());
+		for (Lap lap : laps) {
 			netDuration = netDuration.plus(lap.getNetDuration());
 		}
 		return netDuration;
 	}
-	
+
 	/** {@inheritDoc} */
 	public ImmutableList<TrackPoint> getTrackPoints() {
 		ImmutableList.Builder<TrackPoint> builder = ImmutableList.builder();
-		for (Lap lap: laps) {
+		for (Lap lap : laps) {
 			builder.addAll(lap.getTrackPoints());
 		}
 		return builder.build();
@@ -118,40 +129,40 @@ public final class ActivityImpl implements Comparable<Activity>, Serializable, A
 		Lap lastLap = Iterables.getLast(laps);
 		return Iterables.getLast(lastLap.getTrackPoints());
 	}
-	
+
 	/** {@inheritDoc} */
 	public ImmutableList<Lap> getLaps() {
 		return laps;
 	}
-	
 
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
 	public String toString() {
-		return "Activity [start time=" + startTime + ", #laps=" + laps.size() + "]";
+		return "Activity [start time=" + startTime + ", #laps=" + laps.size()
+				+ "]";
 	}
 
 	/** {@inheritDoc} */
 	public Length getDistance() {
 		return getLastTrackPoint().getDistance();
 	}
-	
+
 	/** {@inheritDoc} */
 	public Length getAltitudeGain() {
 		final Length minimumGain = Length.createLengthInMeters(5.0);
 		Length totalGain = Length.createLengthInMeters(0.0);
-		
+
 		// only count as climb if 5 meters are gained without a drop in between.
-		// This is a crude way  to filter out any noise.
+		// This is a crude way to filter out any noise.
 		LinkedList<TrackPoint> climbingStretch = Lists.newLinkedList();
-		for (TrackPoint trackPoint: getTrackPoints()) {
+		for (TrackPoint trackPoint : getTrackPoints()) {
 			if (trackPoint.getAltitudeDelta().getValueInMeters() > 0.0) {
 				climbingStretch.add(trackPoint);
 			} else {
 				Length gain = Length.createLengthInMeters(0.0);
-				for (TrackPoint climbingTrackPoint: climbingStretch) {
+				for (TrackPoint climbingTrackPoint : climbingStretch) {
 					gain = gain.plus(climbingTrackPoint.getAltitudeDelta());
 				}
 				if (gain.compareTo(minimumGain) > 0) {
@@ -162,7 +173,7 @@ public final class ActivityImpl implements Comparable<Activity>, Serializable, A
 		}
 		return totalGain;
 	}
-	
+
 	/** {@inheritDoc} */
 	@Override
 	public boolean equals(final Object o) {
@@ -177,30 +188,26 @@ public final class ActivityImpl implements Comparable<Activity>, Serializable, A
 		}
 		// compare with interface Activity, not with ActivityImpl!
 		Activity other = (Activity) o;
-		
-		return new EqualsBuilder()
-			.append(other.getStartTime(), startTime)
-			.append(other.getLaps(), laps)
-			.isEquals();	
+
+		return new EqualsBuilder().append(other.getStartTime(), startTime)
+				.append(other.getLaps(), laps).isEquals();
 	}
-	
+
 	/** Seeds for {@link HashCodeBuilder}. */
-	private static final int[] HASH_CODE_SEEDS = new int[] {31, 43};
-	
+	private static final int[] HASH_CODE_SEEDS = new int[] { 31, 43 };
+
 	/** {@inheritDoc} */
 	@Override
 	public int hashCode() {
 		return new HashCodeBuilder(HASH_CODE_SEEDS[0], HASH_CODE_SEEDS[1])
-			.append(startTime)
-			.append(true)
-			.toHashCode();
+				.append(startTime).append(true).toHashCode();
 	}
-	
+
 	/** {@inheritDoc} */
-    @Override
-    public int compareTo(final Activity o) {
-    	checkNotNull(o);
-        
-        return startTime.compareTo(o.getStartTime());
-    }
+	@Override
+	public int compareTo(final Activity o) {
+		checkNotNull(o);
+
+		return startTime.compareTo(o.getStartTime());
+	}
 }
