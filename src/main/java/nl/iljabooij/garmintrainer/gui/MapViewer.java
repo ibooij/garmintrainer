5ab858c04879c72683420e2ee6201e6e07a25dcd
ibooij <ibooij@gmail.com>
2009-10-24 13:27:54 +0200

    [Major] maps zooms to activity again.

--- src/main/java/nl/iljabooij/garmintrainer/gui/MapViewer.java
+++ src/main/java/nl/iljabooij/garmintrainer/gui/MapViewer.java
@@ -29,7 +29,7 @@ import java.awt.geom.Path2D;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.io.File;
-import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.SwingUtilities;
 
@@ -38,30 +38,52 @@ import nl.iljabooij.garmintrainer.model.ApplicationState;
 import nl.iljabooij.garmintrainer.model.TrackPoint;
 import nl.iljabooij.garmintrainer.model.ApplicationState.Property;
 
+import org.openstreetmap.gui.jmapviewer.Coordinate;
 import org.openstreetmap.gui.jmapviewer.JMapViewer;
 import org.openstreetmap.gui.jmapviewer.OsmFileCacheTileLoader;
 import org.openstreetmap.gui.jmapviewer.OsmTileSource;
-import org.openstreetmap.gui.jmapviewer.interfaces.MapMarker;
 
-import com.google.common.collect.Lists;
+import com.google.common.base.Function;
+import com.google.common.base.Predicate;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
 import com.google.inject.Inject;
 
 /**
  * Subclass of {@link JMapViewer} that shows the route of an {@link Activity} as
  * a collection of {@link SmallMapMarker} objects.
+ * 
  * @author ilja
  */
 public class MapViewer extends JMapViewer {
 	private static final long serialVersionUID = 1L;
 	private transient final ApplicationState applicationState;
-	
-	private static final File CACHE_DIR = new File(new File(System.getProperty("user.home"), ".garminTrainer")
-		, "tileCache");
+
+	private static final File CACHE_DIR = new File(new File(System
+			.getProperty("user.home"), ".garminTrainer"), "tileCache");
 
 	/**
-	 * Create a new {@link MapViewer}. Normally, this constructor is called
-	 * from Guice.
-	 * @param applicationState the global application state.
+	 * {@link PropertyChangeListener} for activity changes.
+	 */
+	private PropertyChangeListener activityChangeListener = new PropertyChangeListener() {
+		@Override
+		public void propertyChange(final PropertyChangeEvent evt) {
+			SwingUtilities.invokeLater(new Runnable() {
+				@Override
+				public void run() {
+					zoomMapToActivity(evt.getNewValue());
+					repaint();
+				}
+			});
+		}
+	};
+
+	/**
+	 * Create a new {@link MapViewer}. Normally, this constructor is called from
+	 * Guice.
+	 * 
+	 * @param applicationState
+	 *            the global application state.
 	 */
 	@Inject
 	public MapViewer(final ApplicationState applicationState) {
@@ -71,50 +93,34 @@ public class MapViewer extends JMapViewer {
 		setTileSource(new OsmTileSource.Mapnik());
 		this.applicationState = applicationState;
 		this.applicationState.addPropertyChangeListener(
-				Property.CurrentActivity, new PropertyChangeListener() {
-					@Override
-					public void propertyChange(PropertyChangeEvent evt) {
-						changeMarkers();
-					}
-				});
-		
+				Property.CurrentActivity, activityChangeListener);
 		setTileLoader(new OsmFileCacheTileLoader(this, CACHE_DIR));
 	}
-	
-	/**
-	 * Change the map markers using the track points in the current activity.
-	 */
-	private void changeMarkers() {
-		final ArrayList<MapMarker> markers = Lists.newArrayList();
-		for (TrackPoint trackPoint : applicationState.getCurrentActivity()
-				.getTrackPoints()) {
-			if (trackPoint.hasPosition())
-				markers.add(new SmallMapMarker(trackPoint));
-		}
-
-		updateMap(markers);
-	}
 
 	/**
-	 * Paint the track of the {@link Activity} on the map. The map itself is 
+	 * Paint the track of the {@link Activity} on the map. The map itself is
 	 * painted in {@link JMapViewer}, the superclass of this class.
-	 * @param g the {@link Graphics} object used for drawing.
+	 * 
+	 * @param g
+	 *            the {@link Graphics} object used for drawing.
 	 * @see JMapViewer#paintComponents(Graphics)
 	 */
 	@Override
 	protected void paintComponent(final Graphics g) {
 		super.paintComponent(g);
-		
-		// if there is no current Activity, there is no sense in drawing 
+
+		// if there is no current Activity, there is no sense in drawing
 		// anything.
 		if (applicationState.getCurrentActivity() == null) {
 			return;
 		}
-		
+
 		final Path2D path = new Path2D.Double();
-		for (TrackPoint trackPoint: applicationState.getCurrentActivity().getTrackPoints()) {
+		for (TrackPoint trackPoint : applicationState.getCurrentActivity()
+				.getTrackPoints()) {
 			if (trackPoint.hasPosition()) {
-				Point p = getMapPosition(trackPoint.getLatitude(), trackPoint.getLongitude(), false);
+				Point p = getMapPosition(trackPoint.getLatitude(), trackPoint
+						.getLongitude(), false);
 				if (path.getCurrentPoint() == null) {
 					path.moveTo(p.x, p.y);
 				} else {
@@ -122,38 +128,52 @@ public class MapViewer extends JMapViewer {
 				}
 			}
 		}
-		
+
 		final Graphics2D g2d = (Graphics2D) g;
 		Composite originalComposite = g2d.getComposite();
-	
+
 		// use Alpha factor for drawing.
 		int type = AlphaComposite.SRC_OVER;
-	    g2d.setComposite(AlphaComposite.getInstance(type, 0.5f));
-	    
-		g2d.setStroke(new BasicStroke(8, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
-		
+		g2d.setComposite(AlphaComposite.getInstance(type, 0.5f));
+
+		g2d.setStroke(new BasicStroke(8, BasicStroke.CAP_ROUND,
+				BasicStroke.JOIN_ROUND));
+
 		g2d.setColor(Color.RED);
 		g2d.draw(path);
-		
+
 		// return to original composite
 		g2d.setComposite(originalComposite);
 	}
-	
-	/**
-	 * Update the map using a new list of markers.
-	 * @param markers markers to use in the map.
-	 */ 
-	private void updateMap(final ArrayList<MapMarker> markers) {
-		SwingUtilities.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				// we don't want to show the markers, just use them to set the
-				// region of the map that we want to show.
-				//setMapMarkerVisible(false);
-				//setMapMarkerList(markers);
-				//setDisplayToFitMapMarkers();
-				repaint();
-			}
-		});
+
+	private void zoomMapToActivity(final Object activityObject) {
+		assert(SwingUtilities.isEventDispatchThread());
+		
+		Coordinate northWest = new Coordinate(-180.0, 90.0);
+		Coordinate southEast = new Coordinate(180.0, -90.0);
+		if (activityObject != null) {
+			Activity activity = (Activity) activityObject;
+			Predicate<TrackPoint> trackPointHasPosition = new Predicate<TrackPoint>() {
+				@Override
+				public boolean apply(TrackPoint input) {
+					return input.hasPosition();
+				}
+				
+			};
+			Function<TrackPoint, Coordinate> trackPointToCoordinate = new Function<TrackPoint, Coordinate>() {
+				@Override
+				public Coordinate apply(TrackPoint from) {
+					return new Coordinate(from.getLatitude(), from
+							.getLongitude());
+				}
+			};
+			List<Coordinate> coordinates = ImmutableList.copyOf(Iterables
+					.transform(Iterables.filter(activity.getTrackPoints(),
+							trackPointHasPosition), trackPointToCoordinate));
+			Coordinate[] bounds = Coordinate.getBoundingBox(coordinates);
+			northWest = bounds[0];
+			southEast = bounds[1];
+		}
+		zoomToBoundingBox(northWest, southEast);
 	}
 }
--- src/main/java/org/openstreetmap/gui/jmapviewer/Coordinate.java
+++ src/main/java/org/openstreetmap/gui/jmapviewer/Coordinate.java
@@ -3,10 +3,9 @@ package org.openstreetmap.gui.jmapviewer;
 //License: GPL. Copyright 2009 by Stefan Zeller
 
 import java.awt.geom.Point2D;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 
 /**
  * This class encapsulates a Point2D.Double and provide access
@@ -15,9 +14,8 @@ import java.io.Serializable;
  * @author Jan Peter Stotz
  * 
  */
-public class Coordinate implements Serializable {
-	private static final long serialVersionUID = 1L;
-	private transient Point2D.Double data;
+public class Coordinate {
+	private final Point2D.Double data;
 
 	/**
 	 * Create a new Coordinate with 
@@ -36,18 +34,48 @@ public class Coordinate implements Serializable {
         return data.x;
     }
 
-    private void writeObject(ObjectOutputStream out) throws IOException {
-        out.writeObject(data.x);
-        out.writeObject(data.y);
-    }
-
-    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
-        data = new Point2D.Double();
-        data.x = (Double) in.readObject();
-        data.y = (Double) in.readObject();
-    }
-
     public String toString() {
         return "Coordinate[" + data.y + ", " + data.x + "]";
+    }
+    
+    /**
+     * Get the point between this point and the other one
+     * @param other the other point.
+     * @return point right between this and the other coordinate
+     */
+    public Coordinate getPointBetween(final Coordinate other) {
+    	// use simple linear interpolation. This is not completely accurate,
+    	// but it will do for now.
+    	double lat = (getLat() + other.getLat()) / 2.0;
+    	double lon = (getLon() + other.getLon()) / 2.0;
+    	
+    	return new Coordinate(lat, lon);
+    }
+    
+    public static Coordinate[] getBoundingBox(final Collection<Coordinate> coordinates) {
+    	final Comparator<Coordinate> latitudeComparator = new Comparator<Coordinate>() {
+			@Override
+			public int compare(Coordinate o1, Coordinate o2) {
+				// higher latitudes are further north, so we need to negate the value of compare
+				return - Double.compare(o1.getLat(), o2.getLat());
+			}
+		};
+		final Comparator<Coordinate> longitudeComparator = new Comparator<Coordinate>() {
+			@Override
+			public int compare(Coordinate o1, Coordinate o2) {
+				return Double.compare(o1.getLon(), o2.getLon());
+			}
+		};
+
+		// this completely non-optimal, but easy.. (lazy me)
+    	double northWestLatitude = Collections.min(coordinates, latitudeComparator).getLat();
+    	double southEastLatitude = Collections.max(coordinates, latitudeComparator).getLat();
+    	double northWestLongitude = Collections.min(coordinates, longitudeComparator).getLon();
+    	double southEastLongitude = Collections.max(coordinates, longitudeComparator).getLon();
+ 
+    	final Coordinate northWest = new Coordinate(northWestLatitude, northWestLongitude);
+    	final Coordinate southEast = new Coordinate(southEastLatitude, southEastLongitude);
+    	
+    	return new Coordinate[] {northWest, southEast}; 
     }
 }
