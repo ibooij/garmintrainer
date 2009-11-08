8ad0185ceae93203544baec759caf140229f94f9
Ilja Booij <ibooij@gmail.com>
2009-11-08 14:51:21 +0100

    [Major] more changes to work with Scala.

--- src/main/scala/nl/iljabooij/garmintrainer/NewMain.scala
+++ src/main/scala/nl/iljabooij/garmintrainer/NewMain.scala
@@ -3,38 +3,42 @@ package nl.iljabooij.garmintrainer
 import java.awt.Dimension
 import javax.swing._
 
+import scala.swing.MainFrame
+
 import nl.iljabooij.garmintrainer.gui._
 import nl.iljabooij.garmintrainer._
 
 import com.google.inject.Guice
 import org.jvnet.substance.skin.SubstanceBusinessBlackSteelLookAndFeel
 
+import org.apache.log4j.PropertyConfigurator
 /**
  Just a simple main. 
  */
 object NewMain {
-  val TitlePrefix = "Garmin Trainer"
+  val titlePrefix = "Garmin Trainer"
   
   def main(args:Array[String]) {
     println("Hello World!")
     println(Thread.currentThread)
+    val logConfigUrl = this.getClass.getResource("/log4j.properties")
+    PropertyConfigurator.configure(logConfigUrl)
     SwingUtilities.invokeLater(
+      
       new Runnable {
-
         def run = {
           UIManager.setLookAndFeel(new SubstanceBusinessBlackSteelLookAndFeel)
-          val frame = new JFrame(TitlePrefix);
-          frame.setLocationByPlatform(true);
-          frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+          val frame = new MainFrame
+          frame.title = titlePrefix
           
           val injector = Guice.createInjector(new GuiceModule)
           
           val scalaGui = injector.getInstance(classOf[ScalaGui])
           scalaGui.init(frame)
           
-          frame.setSize(new Dimension(600, 600))
+          frame.size = new Dimension(600, 600)
           frame.pack
-          frame.setVisible(true)
+          frame.visible = true
         }
       }
     )
