74d92cd1e80254363ec17a5a004dfe9a542a2f9a
Ilja Booij <ibooij@gmail.com>
2009-11-20 10:01:57 +0100

    [Major] added scalatest as a dependency. [Major] test class for TcxImporterImpl ported to scala.

---
+++ src/test/scala/nl/iljabooij/garmintrainer/importer/TcxImporterScalaTest.scala
@@ -0,0 +1,70 @@
+package nl.iljabooij.garmintrainer.importer
+
+import java.io.{File,InputStream}
+import scala.collection.jcl.Conversions._
+import org.scalatest.junit.{AssertionsForJUnit,JUnit3Suite}
+import org.scalatest.mock.MockitoSugar
+import org.junit.{Before,Test}
+import org.mockito.Matchers._
+import org.mockito.Mockito._
+import org.hamcrest.core.IsInstanceOf
+import com.google.inject.Provider
+
+import nl.iljabooij.garmintrainer.model.{Activity,ApplicationState}
+import nl.iljabooij.garmintrainer.parser.{TcxParser,ParseException}
+
+
+class TcxImporterScalaTest extends JUnit3Suite with AssertionsForJUnit with MockitoSugar{
+  var tcxImporter: TcxImporter = null
+  var applicationState: ApplicationState = null
+  var tcxParser: TcxParser = null
+  var tcxParserProvider: Provider[TcxParser] = null
+  
+  override def setUp() {
+    applicationState = mock[ApplicationState]
+    tcxParser = mock[TcxParser]
+    
+    tcxParserProvider = mock[Provider[TcxParser]]
+	when(tcxParserProvider.get()).thenReturn(tcxParser);
+ 
+	tcxImporter = new TcxImporterImpl(applicationState, tcxParserProvider)
+  }
+
+  def testFileDoesNotExist() {
+    intercept[TcxImportException] {
+      tcxImporter.importTcx(new File("/ThisFileDoesNotExist"))
+    }
+  }
+  
+  def testImportActivity() {
+     val uri = getClass().getResource("/sample.tcx").toURI()
+     val file = new File(uri)
+     
+     val activities = new java.util.LinkedList[Activity]
+     activities.add(mock[Activity])
+     
+     when(tcxParser.parse(any(classOf[InputStream]))).thenReturn(activities)
+     
+     tcxImporter.importTcx(file)
+     
+     verify(applicationState, times(1)).setCurrentActivity(activities.first)
+     verify(tcxParser, times(1)).parse(any(classOf[InputStream]))
+   }
+   
+   	/**
+	 * Test what happens when the parser throws a ParseException
+	 */
+   def testWithParserThrowingException() {
+     val uri = getClass().getResource("/sample.tcx").toURI()
+     val file = new File(uri);
+     val activities = new java.util.LinkedList[Activity]
+     activities.add(mock[Activity])
+     
+     when(tcxParser.parse(any(classOf[InputStream]))).thenThrow(
+       new ParseException("test exception"))
+      
+     intercept[TcxImportException] {
+	   tcxImporter.importTcx(file);
+     }
+   }
+}
